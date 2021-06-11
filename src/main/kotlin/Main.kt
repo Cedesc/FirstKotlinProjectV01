81325634dee71f32e14a6f2ae22202b192038712
Christian Dohmen <cdohme01@students.uni-mainz.de>
2021-06-11 19:47:12 +0200

    feat: tutorial #30

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -48,6 +48,8 @@ fun main(args: Array<String>) {
 
 
 
+    // Loops
+
     // for loops
     print("\n")
     val myString: String = "Hello World!"
@@ -71,6 +73,48 @@ fun main(args: Array<String>) {
         continue
     } while (false)
 
+    // break and continue
+    var loopResult: String = ""
+    var xLoop: Int = 5
+    customName@while (xLoop > 1) {
+        for (char: Char in "hello darkness") {
+            if (char != 'h' && char != 'e') {
+                break@customName // break of the outer loop
+            }
+            loopResult += char
+        }
+        xLoop--
+        loopResult += "  "
+    }
+    println("Rest from \"hello darkness\": $loopResult")
+
+    // Ranges
+    print("Range from 0 to 20: ")
+    for (i in 0..20) {
+         print("$i ")
+        // The numbers in between and both (!) 0 and 20 are output
+    }
+    print("\nRange from 20 to 0 in steps of 3: ")
+    for (i in 20 downTo 0 step 3) {
+        print("$i ")
+    }
+    print("\n")
+
+    // foreach loop
+    val myForEachArr = arrayOf(42, 4, 42, 24, 2, 4)
+    print("foreach-loop output: ")
+    myForEachArr.forEach {
+        print("$it ") // "it" is the current Iterator in foreach-loops
+    }
+    print("\n")
+
+    // index and element
+    val myOtherArr = arrayOf(1337, 42, 420)
+    for ((index, number) in myOtherArr.withIndex()) {
+        print("Index: $index  Number: $number    ")
+    }
+    print("\n")
+
 
 
 
@@ -88,9 +132,10 @@ fun main(args: Array<String>) {
 
 
     // Maybe Typen
-    var myString1: String? = null
+    print("\n")
+    val myString1: String? = null
     println("Finger weg von meinen Nuggets: " + myString1?.length) // ? only execute if it is not null
-    var myString2 = "Ok"
+    val myString2 = "Ok"
     println("Sooooooo ein Feuerball: " + myString2!!.length) // !! assume that it is not null
 
     println(myString1?.length ?:"String is null") // ?: if previous is null returns following
@@ -104,7 +149,8 @@ fun main(args: Array<String>) {
 
 
     // nullable Booleans
-    var myBool: Boolean? = null
+    print("\n")
+    val myBool: Boolean? = null
     if (myBool == true) { // "if (myBool)" is not good
         println(myBool)
     } else {
@@ -118,6 +164,7 @@ fun main(args: Array<String>) {
 
 
     // Swap variables
+    print("\n")
     var aSwap: Int = 111
     var bSwap: Int = 222
     aSwap = bSwap.also { // inside the brackets, the previous command isn't executed
@@ -129,6 +176,7 @@ fun main(args: Array<String>) {
 
 
     // Typecasts
+    print("\n")
     val myVar1: Int = 1337
     val myVar2: Byte = myVar1.toByte()
     val myVar3: Int = 3
@@ -136,6 +184,24 @@ fun main(args: Array<String>) {
 
     println("After Typecast: $myVar2 and $myVar4")
 
+
+
+
+    // Pattern Matching with "when"
+    print("\n")
+    val xWhen: Int = 42
+    var outputWhen:String = when(xWhen) {
+        0, 1, 2, 3, 4 -> "xWhen is between 0 and 4"
+        in 35..45 -> "xWhen is between 35 and 45"
+        else -> {
+            "xWhen is something"
+        }
+    }
+    println(outputWhen)
+    when{
+        xWhen > 5 -> println("xWhen is greater than 5")
+        xWhen > 100 -> println("xWhen is greater than 100, what is true, but this message won't be displayed")
+    }
 
 
 
